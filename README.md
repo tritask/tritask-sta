e9894b293a7eac117d26bc62e979e8da0cf084a8
stakiran <stakiran@users.noreply.github.com>
2017-07-29 17:00:31 +0900

    add files.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # tritask-sta
-Tritask 実装例。秀丸エディタ + Python2.7 ベース。
+[Tritask](https://github.com/tritask/tritask-spec) のオレオレ実装例です。
+
+![tritasksta_image](https://user-images.githubusercontent.com/23325839/28743090-32fc207c-747c-11e7-81f3-6a9764bffb43.jpg)
+
+<!-- toc -->
+- [tritask-sta](#tritask-sta)
+  - [コンセプト](#コンセプト)
+  - [システム要件](#システム要件)
+  - [インストール](#インストール)
+  - [使い方](#使い方)
+  - [マニュアル](#マニュアル)
+  - [License](#license)
+  - [Author](#author)
+
+## コンセプト
+- 秀丸エディタ上でサクサク動かしたい
+- でも秀丸エディタだけではキツイので Python にも頼った
+- 見積もり機能は要らない
+- でも日付操作は柔軟に行いたい
+- あと見易いよう強調表示したい
+- タスクの細かい並び順は文字を工夫して制御すればいい(例: 先頭が`a`だと`b`より上に表示される)
+
+## システム要件
+- Windows7+
+- Python 2.7
+- 秀丸エディタ
+
+## インストール
+- `git clone https://github.com/tritask/tritask-sta` などで一式をダウンロード
+- 秀丸エディタに [tritask.mac](tritask.mac) をマクロ登録する
+- （推奨）キー割り当てやツールバーへの配置などを設定して素早く呼び出せるようにしておく
+- .trita ファイルを新規する（空ファイルでも良いが [サンプル](sample.trita) もアリ）
+- （推奨）trita ファイル用の強調表示設定をつくる
+  - その他 > ファイルタイプ別の設定 > 設定の一覧
+  - 適当な設定をコピーして trita 用設定をつくる
+  - 秀丸エディタで trita ファイルを開き、trita 用設定を選んだ後、[trita.hilight](trita.hilight) を強調表示設定として読み込む
+
+## 使い方
+tritask.mac マクロを実行するとメニューが表示されるので、実行したい操作を選択します。
+
+## マニュアル
+@todo そのうち作る。
+
+## License
+[MIT License](LICENSE)
+
+## Author
+[stakiran](https://github.com/stakiran)
---
+++ helper.py
@@ -0,0 +1,391 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import os
+import sys
+
+def file2list(filepath):
+    ret = []
+    with open(filepath, 'r') as f:
+        ret = [line.rstrip('\n') for line in f.readlines()]
+    return ret
+
+def list2file(filepath, ls):
+    with open(filepath, 'w') as f:
+        f.writelines(['%s\n' % line for line in ls] )
+
+def is_monday(weekday_val):
+    return weekday_val==0
+
+def is_tuesday(weekday_val):
+    return weekday_val==1
+
+def is_wednesday(weekday_val):
+    return weekday_val==2
+
+def is_thursday(weekday_val):
+    return weekday_val==3
+
+def is_friday(weekday_val):
+    return weekday_val==4
+
+def is_saturday(weekday_val):
+    return weekday_val==5
+
+def is_sunday(weekday_val):
+    return weekday_val==6
+
+def is_weekday(weekday_val):
+    return 0<=weekday_val<=4
+
+def is_weekend(weekday_val):
+    return not(is_weekday(weekday_val))
+
+def dp(msg):
+    if args.debug:
+        print msg
+
+def abort(msg):
+    print 'Error: {0}'.format(msg)
+    os.system('pause')
+    exit(1)
+
+def assert_y(y, lines):
+    if y>=len(lines):
+        abort('Out of range the line number "{0}", Max is "{1}".' \
+              .format(y, len(lines)))
+
+def datestr2dt(datestr):
+    """ @param datestr A string formatted with `YYYY/MM/DD`. """
+    y = int(datestr[0:4])
+    m = int(datestr[5:7])
+    d = int(datestr[8:10])
+    dt = datetime.datetime(y, m, d)
+    return dt
+
+def dt2dowstr(dt):
+    return ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'][dt.weekday()]
+
+def parse_arguments():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-i', '--input', default=None, required=True,
+        help='A input filename.')
+
+    parser.add_argument('--debug', default=False, action='store_true',
+        help='Debug mode. (Show information to debug.)')
+
+    parser.add_argument('-y', default=None, type=int,
+        help='The start line number of a input line. 0-ORIGIN.')
+    parser.add_argument('--y2', default=None, type=int,
+        help='The end line number of a input line. 0-ORIGIN.')
+    parser.add_argument('-d', '--day', default=None, type=int,
+        help='The day count to walk.')
+
+    parser.add_argument('--to-today', default=False, action='store_true',
+        help='Change the current task day to today. MUST: -y')
+    parser.add_argument('--repeat', default=False, action='store_true',
+        help='Walk day in the current task with rep:n param. MUST: -y.')
+    parser.add_argument('--walk', default=False, action='store_true',
+        help='Walk day in the current task. MUST: -y and -d.')
+    parser.add_argument('--sort', default=False, action='store_true',
+        help='Do sort.')
+
+    args = parser.parse_args()
+    return args
+
+class Task:
+    EMPTY_TIME = ' '*5
+
+    def __init__(self, line):
+        #      01234567890123456789012345678
+        fmt = 'M YYYY/MM/DD DOW HH:MM HH:MM '
+        if len(line)<len(fmt):
+            abort('Invalid format, length shortage "{0}".'.format(line))
+
+        if line[1]!=' '  or \
+           line[12]!=' ' or \
+           line[16]!=' ' or \
+           line[22]!=' ' or \
+           line[28]!=' ':
+            abort('Invalid format, wrong space delim pos "{0}".'.format(line))
+
+        self._line = line
+        self._sortmark    = line[0:1]
+        self._date        = line[2:12]
+        self._dow         = line[13:16]
+        self._starttime   = line[17:22]
+        self._endtime     = line[23:28]
+        self._description = line[29:]
+
+        self._determin_sortmark()
+        self._complete_dow_if_nothing()
+
+        # extract all options from description
+        self._options = {}
+        for elm in self._description.split(' '):
+            if elm.find(':')==-1:
+                continue
+            key, value = elm.split(':')
+            self._options[key] = value
+
+    def _complete_dow_if_nothing(self):
+        if len(self._date.strip()) and len(self._dow.strip())==0:
+            dt = datestr2dt(self._date)
+            self._dow = dt2dowstr(dt)
+
+    def _determin_sortmark(self):
+        # [Marks]
+        #   inbo(Mark is a space)
+        # 1 td
+        # 2 tt, ts
+        # 3 tom
+        # 4 ye
+        INBO = ' '
+        TD   = '1'
+        TT   = '2'
+        TS   = TT
+        TOM  = '3'
+        YE   = '4'
+
+        if len(self._date.strip())==0:
+            self._sortmark = INBO
+            return
+
+        dt = datestr2dt(self._date)
+        today = datetime.datetime.today()
+        # today() をそのまま使うと時分秒レベルの差が生じて
+        # >0 や <0 で判定できないため,
+        # 時分秒を省いた datetime オブジェクトを作る必要がある.
+        today_without_time = datetime.datetime(today.year, today.month, today.day)
+
+        delta = dt-today_without_time
+        diff = int(delta.total_seconds())
+        if diff<0:
+            self._sortmark = YE
+            return
+        if diff>0:
+            self._sortmark = TOM
+            return
+
+        s, e = len(self._starttime.strip()), len(self._endtime.strip())
+        if s==0 and e==0:
+            self._sortmark = TT
+            return
+        if s==0 and e:
+            # 終了時間のみは容認しない.
+            # 未記入に戻す.
+            self._sortmark = TT
+            self._endtime = self.EMPTY_TIME
+            return
+        if s and e==0:
+            self._sortmark = TS
+            return
+        self._sortmark = TD
+
+    def hold_me(self):
+        try:
+            holdday = int(self._options['hold'])
+        except (KeyError, ValueError):
+            return
+
+        today = datetime.datetime.today()
+        self._date = '{0}/{1:02d}/{2:02d}'.format(today.year, today.month, today.day)
+        self._dow  = dt2dowstr(today)
+
+        self.walk(holdday)
+
+    def skip_me(self):
+        # 未完了タスクのみ対象にする.
+        if not(self._starttime) or not(self._endtime):
+            return
+
+        y = int(self._date[0:4])
+        m = int(self._date[5:7])
+        d = int(self._date[8:10])
+        dt = datetime.datetime(y, m, d)
+        dt_original = datetime.datetime(y, m, d)
+
+        delta1 = datetime.timedelta(days=1)
+        walk_count = 0
+        ops = self._options
+
+        while True:
+            wd = dt.weekday()
+            do_skip = False
+
+            if ops.get('skipmon') and is_monday(wd) or \
+               ops.get('skiptue') and is_tuesday(wd) or \
+               ops.get('skipwed') and is_wednesday(wd) or \
+               ops.get('skipthu') and is_thursday(wd) or \
+               ops.get('skipfri') and is_friday(wd) or \
+               ops.get('skipsat') and is_saturday(wd) or \
+               ops.get('skipsun') and is_sunday(wd) or \
+               ops.get('skipweekday') and is_weekday(wd) or \
+               ops.get('skipweekend') and is_weekend(wd):
+                do_skip = True
+
+            if not(do_skip):
+                break
+
+            walk_count += 1
+            dt = dt + delta1
+            if walk_count>7:
+                # 一周した = 永遠にスキップされ続ける,
+                # なので無効にする. とりあえずスキップ無しにしとく.
+                dt = dt_original
+                break
+
+        self._date = '{0}/{1:02d}/{2:02d}'.format(dt.year, dt.month, dt.day)
+        self._dow  = dt2dowstr(dt)
+
+    def repeat_me(self):
+        try:
+            repday = int(self._options['rep'])
+        except (KeyError, ValueError):
+            return
+
+        self.walk(repday)
+
+    def to_today(self):
+        today = datetime.datetime.today()
+        self._date = '{0}/{1:02d}/{2:02d}'.format(today.year, today.month, today.day)
+        self._dow  = dt2dowstr(today)
+
+    def walk(self, day):
+        # 0123456789
+        # YYYY/MM/DD
+        y = int(self._date[0:4])
+        m = int(self._date[5:7])
+        d = int(self._date[8:10])
+
+        dt = datetime.datetime(y, m, d)
+        delta = datetime.timedelta(days=abs(day))
+
+        if day>=0:
+            newdt = dt + delta
+        else:
+            newdt = dt - delta
+
+        self._date = '{0}/{1:02d}/{2:02d}'.format(newdt.year, newdt.month, newdt.day)
+        self._dow  = dt2dowstr(newdt)
+
+    def __str__(self):
+        return '{0} {1} {2} {3} {4} {5}'.format(
+            self._sortmark, self._date, self._dow,
+            self._starttime, self._endtime,
+            self._description
+        )
+
+    def print_options(self):
+        for k, v in self._options.items():
+            print '{0}: [{1}]'.format(k, v)
+
+def apply_holding(lines):
+    for i, line in enumerate(lines):
+        if line.find('hold:')==-1:
+            continue
+        task = Task(line)
+        task.hold_me()
+        lines[i] = str(task)
+
+def apply_skipping(lines):
+    for i, line in enumerate(lines):
+        if line.find('skip')==-1:
+            continue
+        task = Task(line)
+        task.skip_me()
+        lines[i] = str(task)
+
+def apply_completion(lines):
+    for i, line in enumerate(lines):
+        task = Task(line)
+        lines[i] = str(task)
+
+args = parse_arguments()
+
+MYDIR = os.path.abspath(os.path.dirname(__file__))
+infile = os.path.join(MYDIR, args.input)
+lines = file2list(infile)
+
+if args.debug and args.y!=None:
+    task = Task(lines[args.y])
+    print task
+    task.print_options()
+
+if args.walk:
+    y = args.y
+    y2 = args.y2
+    if y2==None:
+        y2 = y
+    assert_y(y, lines)
+    assert_y(y2, lines)
+    day = args.day
+
+    for cnt in range(y2-y+1):
+        targetidx = cnt + y
+        line = lines[targetidx]
+        task = Task(line)
+        task.walk(day)
+        lines[targetidx] = str(task)
+
+    outfile = infile
+    list2file(outfile, lines)
+    exit(0)
+
+if args.repeat:
+    y = args.y
+    assert_y(y, lines)
+
+    line = lines[y]
+    task = Task(line)
+    task.repeat_me()
+
+    lines[y] = str(task)
+
+    outfile = infile
+    list2file(outfile, lines)
+    exit(0)
+
+if args.to_today:
+    y = args.y
+    y2 = args.y2
+    if y2==None:
+        y2 = y
+    assert_y(y, lines)
+    assert_y(y2, lines)
+    day = args.day
+
+    for cnt in range(y2-y+1):
+        targetidx = cnt + y
+        line = lines[targetidx]
+        task = Task(line)
+        task.to_today()
+        lines[targetidx] = str(task)
+
+    outfile = infile
+    list2file(outfile, lines)
+    exit(0)
+
+if args.sort:
+    outfile = infile
+
+    # before sorting
+    # --------------
+    apply_holding(lines)
+    apply_skipping(lines)
+    apply_completion(lines)
+
+    # sorting
+    # -------
+    lines.sort()
+
+    # after sorthing
+    # --------------
+    pass
+
+    list2file(outfile, lines)
+    exit(0)
